d5b7d1456768c73dbd24e6e75a00783bfe50323f
Aleksey Emelyantsev <aleks.twin@gmail.com>
2023-04-24 20:04:23 +0300

    C++20 module usage (#6)
    * Add modules usage examples
    
    * Add more modules usage examples: partitions and templates

---
+++ 01_CoreLanguage/23_mathModule/23_mathModule/mathInterfaceUnit.ixx
@@ -0,0 +1,13 @@
+module;
+
+#include <vector>
+
+export module math;
+
+export namespace math {
+
+int add( int fir, int sec );
+
+int getProduct( const std::vector<int>& vec );
+
+}
---
+++ 01_CoreLanguage/31_clientTemplate/clientTemplate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+import math;
+
+int main()
+{
+
+    std::cout << '\n';
+
+    std::cout << "math::sum(2000, 11): " << math::sum( 2000, 11 ) << '\n';
+
+    std::cout << "math::sum(2013.5, 0.5): " << math::sum( 2013.5, 0.5 ) << '\n';
+
+    std::cout << "math::sum(2017, false): " << math::sum( 2017, false ) << '\n';
+
+}
