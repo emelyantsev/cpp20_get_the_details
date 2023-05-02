1382fb68999031db3cb6043f4f0b37128c83d6bc
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2023-05-02 13:39:35 +0300

    Add ranges part

---
+++ 02_StandardLibrary/203_sortRanges/sortRanges.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+
+    std::vector<int> myVec{ -3, 5, 0, 7, -4 };
+    std::ranges::sort( myVec );
+    for( auto v : myVec ) std::cout << v << " "; // -4, -3, 0, 5, 7
+
+}
