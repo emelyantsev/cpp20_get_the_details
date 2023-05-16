8485d262ba08c8d2d5ace1088563964dc796e813
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2023-05-16 15:29:36 +0300

    Add atomics part

---
+++ 03_Concurrency/307_atomicRefCopy/atomicRefCopy.cpp
@@ -0,0 +1,18 @@
+#include <atomic>
+#include <iostream>
+
+int main()
+{
+
+    std::cout << '\n';
+
+    int val{ 5 };
+    int& ref = val;
+    std::atomic<int> atomicRef( ref );
+    ++atomicRef;
+    std::cout << "ref: " << ref << '\n';
+    std::cout << "atomicRef.load(): " << atomicRef.load() << '\n';
+
+    std::cout << '\n';
+
+}
